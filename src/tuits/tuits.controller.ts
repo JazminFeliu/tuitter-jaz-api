6561d79e2989aae0263f1110832190cd82daaae7
JazminFeliu <jazminfeliu@gmail.com>
2022-03-14 12:52:29 -0300

    subo service y armo controllers

--- src/tuits/tuits.controller.ts
+++ src/tuits/tuits.controller.ts
@@ -1,15 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Patch, Post, Delete, Query } from '@nestjs/common';
+import { Tuit } from './tuit.entity';
+import { TuitsService } from './tuits.service';
 
 //decoradores se llaman los que se anteceden con el arroba
 @Controller('tuits')
 export class TuitsController {
+    constructor(private readonly tuitService: TuitsService){
+
+    }
+
     @Get()
-    getTuits() {
-        return 'Hello from Tuitter-Jaz';
+    getTuits(@Query() filterQuery): Tuit[] {
+        const { searchTerm, orderBy } = filterQuery;
+        return this.tuitService.getTuits();
     }
 
     @Get(':id') //tuits/1
-    getTuit(@Param('id') id: string): string {
-        return `Your tuit id is ${id}`;
+    getTuit(@Param('id') id: string): Tuit {
+        return this.tuitService.getTuit(id);
     }
+
+    @Post()
+    createTuit(@Body('message') message: string): void {
+        return this.tuitService.createTuit(message);
+    }
+
+    @Patch(':id')
+    updateTuit(@Param('id') id: string, @Body('message') tuit): Tuit {
+        return this.tuitService.updateTuit(id, tuit);
+    }
+
+    @Delete(':id')
+    removeTuit(@Param('id') id: string): void {
+        return this.tuitService.removeTuit(id);
+    }
+    
 }
