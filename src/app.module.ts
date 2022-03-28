defe1358711a1d6fc8735197aa12ae63833e13e3
JazminFeliu <jazminfeliu@gmail.com>
2022-03-28 13:26:48 -0300

    configuro enum data provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TuitsModule } from './modules/tuits/tuits.module';
 import { UsersModule } from './modules/users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
-    TuitsModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
-    autoLoadEntities: true,
-    synchronize: true
-  }), UsersModule],
+    DatabaseModule,
+    TuitsModule, 
+    UsersModule
+   ],
 })
 export class AppModule {
   static port: number;
